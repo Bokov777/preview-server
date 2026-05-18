ef03f511864d88ef6e3e36ad45d3100ede74e78e
Sergey Brin <7bokov@gmail.com>
2026-05-18 02:08:32 +0200

    add package.json

--- api/preview.js
+++ api/preview.js
@@ -1,3 +1,4 @@
-module.exports = function(req, res) {
-  res.status(200).end('<!DOCTYPE html><html><head><meta property="og:title" content="test"><meta property="og:url" content="https://t.me/web_news_web"><meta http-equiv="refresh" content="0;url=https://t.me/web_news_web"></head><body>OK</body></html>');
+const handler = (req, res) => {
+  res.status(200).send('ok');
 };
+module.exports = handler;
