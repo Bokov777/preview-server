0eb38e1c805f82240f2bf2ffa7da15e35ba2ae7e
Sergey Brin <7bokov@gmail.com>
2026-05-18 03:16:58 +0200

    edge runtime

--- api/preview.js
+++ api/preview.js
@@ -1,4 +1,21 @@
-const handler = (req, res) => {
-  res.status(200).send('ok');
+export default (req, res) => {
+  const id = req.query.id || '';
+  const title = id ? `📰 #${id}` : '📰 Новости';
+  const url = 'https://t.me/web_news_web';
+  
+  const html = `<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta property="og:title" content="${title}">
+  <meta property="og:url" content="${url}">
+  <meta property="og:type" content="article">
+  <meta http-equiv="refresh" content="0;url=${url}">
+</head>
+<body>OK ${id}</body>
+</html>`;
+  
+  return new Response(html, {
+    headers: { 'Content-Type': 'text/html; charset=utf-8' }
+  });
 };
-module.exports = handler;
