ba306d4802bfdf3cb590ed9c7c36636cf247c337
Sergey Brin <7bokov@gmail.com>
2026-05-18 02:06:11 +0200

    minimal

--- api/preview.js
+++ api/preview.js
@@ -1,12 +1,5 @@
-module.exports = (req, res) => {
-  res.status(200).send(`<!DOCTYPE html>
-<html lang="ru">
-<head><meta charset="UTF-8">
-<meta property="og:title" content="Тестовое превью">
-<meta property="og:description" content="Проверка работы Vercel">
-<meta property="og:url" content="https://t.me/web_news_web">
-<meta property="og:type" content="article">
-<meta http-equiv="refresh" content="0;url=https://t.me/web_news_web">
-<title>Тест</title>
-</head><body>OK</body></html>`);
+module.exports = function(request, response) {
+  response.statusCode = 200;
+  response.setHeader('content-type', 'text/html; charset=utf-8');
+  response.end('<!DOCTYPE html><html><body><h1>Test</h1></body></html>');
 };
